f25e4b0c195f295d9b49c24ca69ce36b842b2d11
Phan Huynh <p7huynh@ucsd.edu>
2018-01-25 09:34:37 -0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("OMG My code works and idk why! :O");
+		$("#testjs").text("Please wait...");
+		// why signge vs double???
+		$(".jumbotron p").addClass("active");
+		
 	});
 
 	// Add any additional listeners here
+	$("a.thumbnail").click(projectClick);
 	// example: $("#div-id").click(functionToCall);
+
+}
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    //$(this).css("background-color", "#7fff00");
+
+    //add
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeToggle("slow","linear");
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
